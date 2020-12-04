6d34855065fbe6da54ecff3661af4d0f656ea870
Ean Dalin <eandalined@gmail.com>
2020-12-04 10:33:35 +0700

    add utils in admin

---
+++ src/main/java/com/ecode/core/constant/ErrorCode.java
@@ -0,0 +1,7 @@
+package com.ecode.core.constant;
+
+public interface ErrorCode {
+	public String EXCEPTION_ERR = "exception_error";
+    public  String STATUS       = "status";
+    public String USER_ID       = "user_id";
+}
